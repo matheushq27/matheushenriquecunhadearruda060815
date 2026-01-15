357f12a6ac385b4a322ae0d15772fdfa944a1e5a
Matheus Arruda <matheus.arruda@gmail.com>
2026-01-15 14:37:08 -0400

    Criando service de autenticação

---
+++ src/services/authenticate.service.ts
@@ -0,0 +1,10 @@
+import type { AuthenticateRequest, AuthenticateResponse } from "@/interfaces/services/authenticate.service";
+import api from "./api";
+
+export async function authenticate({ username, password }: AuthenticateRequest) {
+    const response = await api.post("/autenticacao/login", {
+        username,
+        password,
+    });
+    return response.data as AuthenticateResponse;
+}
