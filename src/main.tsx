56d725d03856340a83c9658c6c3295c060b3ded7
Matheus Arruda <matheus.arruda@gmail.com>
2026-01-15 12:12:27 -0400

    Reconfigurando tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
-import 'primeicons/primeicons.css'; //icons
-import "./App.css";
 import App from './App.tsx'
 import { PrimeReactProvider } from "primereact/api";
 import Tailwind from 'primereact/passthrough/tailwind';
-
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css'; //icons
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <BrowserRouter>
-       <PrimeReactProvider value={{ unstyled: false, pt: Tailwind }}>
+       <PrimeReactProvider value={{ unstyled: false, pt: Tailwind }} >
         <App />
     </PrimeReactProvider>
     </BrowserRouter>
