502e83615a401c81c20639ea1cdaae2ef3149fb5
Matheus Arruda <matheus.arruda@gmail.com>
2026-01-16 08:48:21 -0400

    Atualizando tipagem da AuthStore

--- src/interfaces/stores/auth.store.d.ts
+++ src/interfaces/stores/auth.store.d.ts
@@ -1,16 +1,22 @@
 export interface AuthUser {
-  username: string;
-  accessToken: string;
-  refreshToken: string;
-  expireIn: number;
-  refreshExpireIn: number;
+  username: string
+  accessToken: string
+  refreshToken: string
+  expireIn: number
+  refreshExpireIn: number
 }
 
 export interface AuthState {
-  isAuthenticated: boolean;
-  isLoading: boolean;
-  user: AuthUser | null;
+  isAuthenticated: boolean
+  isLoading: boolean
+  user: AuthUser | null
 
-  setUser: (user: any) => void;
-  logout: () => void;
+  setUser: (user: AuthUser) => void
+  setTokens: (
+    accessToken: string,
+    refreshToken: string,
+    expireIn?: number,
+    refreshExpireIn?: number
+  ) => void
+  logout: () => void
 }
