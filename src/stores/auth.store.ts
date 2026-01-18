e3dcd9f03094dfe1c0da0c2e248815c481c39eba
Matheus Arruda <matheus.arruda@gmail.com>
2026-01-18 13:09:08 -0400

    Implementando setTokens

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,20 +1,42 @@
-import type { AuthState } from '@/interfaces/stores/auth.store'
+import type { AuthState, AuthUser } from '@/interfaces/stores/auth.store'
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
 export const useAuthStore = create<AuthState>()(
   persist(
-    (set) => ({
+    (set, get) => ({
       isAuthenticated: false,
       isLoading: false,
       user: null,
 
-      setUser: (user) =>
+      setUser: (user: AuthUser) =>
         set({
           user,
           isAuthenticated: true,
           isLoading: false,
         }),
+
+      setTokens: (
+        accessToken: string,
+        refreshToken: string,
+        expireIn?: number,
+        refreshExpireIn?: number
+      ) => {
+        const currentUser = get().user
+
+        if (!currentUser) return
+
+        set({
+          user: {
+            ...currentUser,
+            accessToken,
+            refreshToken,
+            expireIn: expireIn ?? currentUser.expireIn,
+            refreshExpireIn:
+              refreshExpireIn ?? currentUser.refreshExpireIn,
+          },
+        })
+      },
 
       logout: () =>
         set({
