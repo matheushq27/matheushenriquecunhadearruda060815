3ebbe2e1d6110bfe0c6dc22877e51ee27ed7e792
Matheus Arruda <matheus.arruda@gmail.com>
2026-01-18 13:08:13 -0400

    Criando a função de refreshToken

--- src/services/authenticate/authenticate.service.ts
+++ src/services/authenticate/authenticate.service.ts
@@ -1,5 +1,6 @@
-import type { AuthenticateRequest, AuthenticateResponse } from "@/interfaces/services/authenticate.service";
+import type { AuthenticateRequest, AuthenticateResponse, RefreshTokenResponse } from "@/interfaces/services/authenticate.service";
 import api from "../api";
+import axios from "axios";
 
 export async function authenticate({ username, password }: AuthenticateRequest) {
     const response = await api.post("/autenticacao/login", {
@@ -7,4 +8,23 @@ export async function authenticate({ username, password }: AuthenticateRequest) 
         password,
     });
     return response.data as AuthenticateResponse;
+}
+
+export async function refreshToken({ refreshToken }: { refreshToken: string }) {
+    const response = await axios.put(
+        `${import.meta.env.VITE_API_URL}/autenticacao/refresh`,
+        {},
+        {
+            headers: {
+                Authorization: `Bearer ${refreshToken}`,
+            },
+        }
+    );
+
+    return {
+        accessToken: response.data.access_token,
+        refreshToken: response.data.refresh_token,
+        expiresIn: response.data.expires_in,
+        refreshExpiresIn: response.data.refresh_expires_in,
+    }
 }
