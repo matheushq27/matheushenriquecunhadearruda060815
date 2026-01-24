781063c19a7d0e0ef0cb4bdcb72c834593551346
Matheus Arruda <matheus.arruda@gmail.com>
2026-01-24 14:21:13 -0400

    Adicionando componente ConfirmDialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import AppRoutes from "./routes/AppRoutes";
 import { ToastProvider } from "./contexts/ToastContext";
-
+import { ConfirmDialog } from 'primereact/confirmdialog';
 function App() {
 
   return (
     <ToastProvider>
+      <ConfirmDialog />
       <AppRoutes />
     </ToastProvider>
   );
