c708dbda56dd89abc86b6414b4e263a9ebdb1d02
Matheus Arruda <matheus.arruda@gmail.com>
2026-01-24 14:24:23 -0400

    Criando botões de editar e excluir

--- src/components/CardView/CardView.tsx
+++ src/components/CardView/CardView.tsx
@@ -1,18 +1,45 @@
 import { Avatar } from "primereact/avatar"
+import { Button } from "primereact/button"
 import type { ReactNode } from "react"
-export function CardView({ avatar, title, subtitle, content }: { avatar: string, title: string, subtitle: string, content?: ReactNode }) {
+
+interface CardViewProps {
+    avatar: string
+    title: string
+    subtitle: string
+    content?: ReactNode
+    onEdit?: () => void
+    onDelete?: () => void
+    loadingDelete?: boolean
+}
+
+export function CardView({ avatar, title, subtitle, content, onEdit, onDelete, loadingDelete }: CardViewProps) {
+
+    const handleEdit = () => {
+        onEdit?.()
+    }
+
+    const handleDelete = () => {
+        onDelete?.()
+    }
+
     return (
-        <div className="p-4 bg-white shadow-sm rounded-md">
-            <div className="flex gap-5">
-                <Avatar image={avatar} size="large" shape="circle" />
-                <div className="min-w-0">
-                    <h2 className="font-bold break-words">{title}</h2>
-                    <p className="text-sm text-gray-500">{subtitle}</p>
+        <div className="bg-white shadow-sm rounded-md flex flex-col justify-between">
+            <div className="p-4">
+                <div className="flex gap-5">
+                    <Avatar image={avatar} size="large" shape="circle" />
+                    <div className="min-w-0">
+                        <h2 className="font-bold break-words">{title}</h2>
+                        <p className="text-sm text-gray-500">{subtitle}</p>
+                    </div>
                 </div>
+                {content && (
+                    <div className="mt-4">{content}</div>
+                )}
             </div>
-            {content && (
-                <div className="mt-4">{content}</div>
-            )}
+            <div className="flex justify-end gap-2 p-2">
+                <Button onClick={handleEdit} icon="pi pi-pencil" className="w-full" severity="info" size="small" text disabled={loadingDelete} />
+                <Button onClick={handleDelete} icon="pi pi-trash" className="w-full" severity="danger" size="small" text loading={loadingDelete} />
+            </div>
         </div>
     )
 }
