3923f68cc1cef550e2e960324dc3d481745ec7b0
Matheus Arruda <matheus.arruda@gmail.com>
2026-01-24 14:37:35 -0400

    Criando NoRecordsFound

---
+++ src/components/NoRecordsFound/NoRecordsFound.tsx
@@ -0,0 +1,5 @@
+export default function NoRecordsFound() {
+    return (
+        <div className="text-center text-gray-500 h-96 flex items-center justify-center">Nenhum registro encontrado.</div>
+    )
+}
