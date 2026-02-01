302334d18d11d1b736ad3b237dfdb460356d76fc
Matheus Arruda <matheus.arruda@gmail.com>
2026-02-01 15:11:09 -0400

    Implementando Lazy loading

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import { PrivateRoutes } from './PrivateRoutes';
 import { PublicRoutes } from './PublicRoutes';
 import Login from "@/pages/Login";
 import { AuthLayout } from '@/layouts/AuthLayout'
 import { GuestLayout } from '@/layouts/GuestLayout'
-import Tutors from '@/pages/Tutors';
 import { NotFound } from '@/pages/NotFound';
-import Pets from '@/pages/Pets';
+import { SectionLoading } from '@/components/SectionLoading';
+
+// Lazy loading dos módulos principais
+const Tutors = lazy(() => import('@/pages/Tutors'));
+const Pets = lazy(() => import('@/pages/Pets'));
 
 export default function AppRoutes() {
     return (
@@ -21,8 +25,17 @@ export default function AppRoutes() {
             {/* Rotas privadas */}
             <Route element={<PrivateRoutes />}>
                 <Route element={<AuthLayout />}>
-                    <Route path="/tutors" element={<Tutors />} />
-                    <Route path="/pets" element={<Pets />} />
+                    {/* Lazy loading para módulos Pets e Tutores */}
+                    <Route path="/tutors" element={
+                        <Suspense fallback={<SectionLoading loading={true} />}>
+                            <Tutors />
+                        </Suspense>
+                    } />
+                    <Route path="/pets" element={
+                        <Suspense fallback={<SectionLoading loading={true} />}>
+                            <Pets />
+                        </Suspense>
+                    } />
                     {/* 404 */}
                     <Route path="*" element={<NotFound />} />
                 </Route>
